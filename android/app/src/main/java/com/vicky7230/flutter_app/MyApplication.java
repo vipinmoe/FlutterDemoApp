95f70973d70216178dfde9db86773ebafee9941b
Vipin Kumar <vipin.kumar@moengage.com>
2020-11-14 11:12:04 +0530

    added geofence

--- android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
+++ android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
@@ -1,9 +1,14 @@
 package com.vicky7230.flutter_app;
 
+import android.content.Intent;
+import android.util.Log;
+
 import com.moengage.core.Logger;
 import com.moengage.core.MoEngage;
 import com.moengage.core.MoEngage.Builder;
 import com.moengage.flutter.MoEInitializer;
+import com.moengage.geofence.MoEGeofenceHelper;
+import com.moengage.geofence.listener.OnGeofenceHitListener;
 
 import io.flutter.app.FlutterApplication;
 
@@ -18,8 +23,17 @@ public class MyApplication extends FlutterApplication {
                 .optOutDefaultInAppDisplay()
                 .setLogLevel(Logger.VERBOSE)
                 .enableMultipleNotificationInDrawer()
+                .enableLocationServices()
                 .optOutTokenRegistration();
 
         MoEInitializer.initialize(getApplicationContext(), builder);
+
+        MoEGeofenceHelper.getInstance().registerGeofenceHitListener(new OnGeofenceHitListener() {
+            @Override
+            public boolean geofenceHit(Intent geoFenceHit) {
+                Log.e("GEOFENCE", "Geofence : " + geoFenceHit.toString());
+                return false;
+            }
+        });
     }
 }
