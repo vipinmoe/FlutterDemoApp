c8fe33aff8de659b9ea45aeac267447bb54531eb
Vipin Kumar <vipin.kumar@moengage.com>
2020-11-14 12:03:57 +0530

    add mi push

--- android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
+++ android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
@@ -3,12 +3,18 @@ package com.vicky7230.flutter_app;
 import android.content.Intent;
 import android.util.Log;
 
+import android.content.Context;
+
 import com.moengage.core.Logger;
 import com.moengage.core.MoEngage;
 import com.moengage.core.MoEngage.Builder;
 import com.moengage.flutter.MoEInitializer;
 import com.moengage.geofence.MoEGeofenceHelper;
 import com.moengage.geofence.listener.OnGeofenceHitListener;
+
+import com.moengage.mi.MoEMiPushHelper;
+import com.moengage.mi.listener.MiPushEventListener;
+import com.xiaomi.mipush.sdk.MiPushMessage;
 
 import io.flutter.app.FlutterApplication;
 
@@ -18,6 +24,7 @@ public class MyApplication extends FlutterApplication {
     public void onCreate() {
         super.onCreate();
         MoEngage.Builder builder = new Builder(this, "U8RR6TSZPEM5EWFBCZBNJVIJ")
+                .configureMiPush("2882303761518810740", "5881881071740", true)
                 .setNotificationSmallIcon(R.drawable.ic_snow)
                 .setNotificationLargeIcon(R.drawable.download)
                 .optOutDefaultInAppDisplay()
@@ -33,6 +40,23 @@ public class MyApplication extends FlutterApplication {
             public boolean geofenceHit(Intent geoFenceHit) {
                 Log.e("GEOFENCE", "Geofence : " + geoFenceHit.toString());
                 return false;
+            }
+        });
+
+        MoEMiPushHelper.Companion.getInstance().setEventListener(new MiPushEventListener(){
+            @Override
+            public void onNonMoEngageNotificationClicked(Context context, MiPushMessage message) {
+                super.onNonMoEngageNotificationClicked(context, message);
+            }
+
+            @Override
+            public void onNonMoEngagePassThroughMessage(Context context,MiPushMessage message) {
+                super.onNonMoEngagePassThroughMessage(context, message);
+            }
+
+            @Override
+            public void onTokenAvailable(String token) {
+                Log.e("TOKEN","Received Mi Push Token : "+token);
             }
         });
     }
