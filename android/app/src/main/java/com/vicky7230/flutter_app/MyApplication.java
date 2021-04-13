431ee389ee360881f762b20be08953652137b32b
Vipin Kumar <vipin.kumar@moengage.com>
2021-04-13 13:07:49 +0530

    cjanges

--- android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
+++ android/app/src/main/java/com/vicky7230/flutter_app/MyApplication.java
@@ -1,17 +1,16 @@
 package com.vicky7230.flutter_app;
 
-import android.content.Intent;
+import android.content.Context;
 import android.util.Log;
 
-import android.content.Context;
-
-import com.moengage.core.Logger;
+import com.moengage.core.LogLevel;
 import com.moengage.core.MoEngage;
 import com.moengage.core.MoEngage.Builder;
+import com.moengage.core.config.FcmConfig;
+import com.moengage.core.config.InAppConfig;
+import com.moengage.core.config.LogConfig;
+import com.moengage.core.config.NotificationConfig;
 import com.moengage.flutter.MoEInitializer;
-import com.moengage.geofence.MoEGeofenceHelper;
-import com.moengage.geofence.listener.OnGeofenceHitListener;
-
 import com.moengage.mi.MoEMiPushHelper;
 import com.moengage.mi.listener.MiPushEventListener;
 import com.xiaomi.mipush.sdk.MiPushMessage;
@@ -24,41 +23,55 @@ public class MyApplication extends FlutterApplication {
     public void onCreate() {
         super.onCreate();
         MoEngage.Builder builder = new Builder(this, "U8RR6TSZPEM5EWFBCZBNJVIJ")
-                .configureMiPush("2882303761518810740", "5881881071740", true)
-                .enablePushKitTokenRegistration()
-                .setNotificationSmallIcon(R.drawable.ic_snow)
-                .setNotificationLargeIcon(R.drawable.download)
-                .optOutDefaultInAppDisplay()
-                .setLogLevel(Logger.VERBOSE)
-                .enableLogsForSignedBuild()
-                .enableMultipleNotificationInDrawer()
-                .enableLocationServices()
-                .optOutTokenRegistration();
+                .configureNotificationMetaData(
+                        new NotificationConfig(
+                                R.drawable.ic_snow,
+                                R.drawable.download,
+                                -1,
+                                null,
+                                true,
+                                true,
+                                true
+                        )
+                )
+                .configureFcm(new FcmConfig(
+                        false
+                ))
+                .configureInApps(new InAppConfig(
+                        false,
+                        null
+                ))
+                .configureLogs(
+                        new LogConfig(
+                                LogLevel.VERBOSE,
+                                true
+                        )
+                );
 
         MoEInitializer.initialize(getApplicationContext(), builder);
 
-        MoEGeofenceHelper.getInstance().registerGeofenceHitListener(new OnGeofenceHitListener() {
+        /*MoEGeofenceHelper.getInstance().registerGeofenceHitListener(new OnGeofenceHitListener() {
             @Override
             public boolean geofenceHit(Intent geoFenceHit) {
                 Log.e("GEOFENCE", "Geofence : " + geoFenceHit.toString());
                 return false;
             }
-        });
+        });*/
 
-        MoEMiPushHelper.Companion.getInstance().setEventListener(new MiPushEventListener(){
+        MoEMiPushHelper.Companion.getInstance().setEventListener(new MiPushEventListener() {
             @Override
             public void onNonMoEngageNotificationClicked(Context context, MiPushMessage message) {
                 super.onNonMoEngageNotificationClicked(context, message);
             }
 
             @Override
-            public void onNonMoEngagePassThroughMessage(Context context,MiPushMessage message) {
+            public void onNonMoEngagePassThroughMessage(Context context, MiPushMessage message) {
                 super.onNonMoEngagePassThroughMessage(context, message);
             }
 
             @Override
             public void onTokenAvailable(String token) {
-                Log.e("TOKEN","Received Mi Push Token : "+token);
+                Log.e("TOKEN", "Received Mi Push Token : " + token);
             }
         });
     }
